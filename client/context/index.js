7ecb8dd293ac3c16bc0e4eacdc684910b3b2e0c8
TgkCapture <tawongakanyenda5@gmail.com>
2023-06-01 12:07:14 +0200

    fixes

--- client/context/index.js
+++ client/context/index.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useContext, createContext} from "react";
+import React, {useState, useEffect, useContext, createContext} from "react";
 
-import {useAddress, useContract, useMetamask, useContractWrite, useContractRead, useContractEvents, } from "@thirdweb-dev/react";
+import {useAddress, useContract, useMetamask, useContractWrite, useContractRead, useContractEvents, useDisconnect, useSigner, useConnectionStatus, } from "@thirdweb-dev/react";
 
 import { ethers } from "ethers";
 
@@ -12,10 +12,18 @@ export const StateContextProvider = ({ children }) => {
     const address = useAddress();
     const connect = useMetamask();
 
-    const realEstate = "Smart E Dapp";
+    // front-end
+    const disconnect = useDisconnect();
+    const ConnectionStatus = useConnectionStatus();
+    const signer = useSigner();
+
+    const [userBalance, setUserBalance] = useState();
+     
+
+    
 
     // 1:list property
-    const { mutateAsync: listProperty, isLoading} = useContractWrite(contract, "listProperty");
+    // const { mutateAsync: listProperty} = useContractWrite(contract, "listProperty");
 
     const createPropertyFunction = async (form) => {
         const {
@@ -28,103 +36,199 @@ export const StateContextProvider = ({ children }) => {
         } = form;
 
         try {
-        const data = await listProperty({ args: [address, price, propertyTitle, category, images, propertyAddress, description], });
-        console.info("contract call successs", data);
-        } catch (err) {
-        console.error("contract call failure", err);
+            const listingPrice = await contract.call("listingPrice");
+            const data = await contract.call("listProperty", [
+              address,
+              price,
+              propertyTitle,
+              category,
+              images,
+              propertyAddress,
+              description,
+            ]);
+      
+            console.info("contract call successs", data);
+            window.location.reload();
+          } catch (err) {
+            console.error("contract call failure", err);
         }
-    } 
+    }; 
 
     // 2:update property
-    const { mutateAsync: updateProperty, isLoading: updatePropertyLoading } = useContractWrite(contract, "updateProperty");
+    const { mutateAsync: updateProperty} = useContractWrite(contract, "updateProperty");
 
     const updatePropertyFunction = async(from) => {
         const {productId, propertyTitle, description, category, images, propertyAddress} = from;
 
         try {
             const data = await updateProperty({
-                args: [
-                    address,
-                    productId,
-                    propertyTitle,
-                    category,
-                    images,
-                    propertyAddress,
-                    description,
-                ],
+              args: [
+                address,
+                productId,
+                propertyTitle,
+                category,
+                images,
+                propertyAddress,
+                description,
+              ],
             });
-
-            console.log("Contract call successfully update", data);
-        } catch (error) {
-            console.log("Error while updating", error);
-        }
+            console.info("contract call successs", data);
+            window.location.reload();
+          } catch (err) {
+            console.error("contract call failure", err);
+          }
     };
 
     // 3:update price
-    const { mutateAsync: updatePrice, isLoading: updatePriceLoading } = useContractWrite(contract, "updatePrice");
+    const { mutateAsync: updatePrice } = useContractWrite(contract, "updatePrice");
 
     const updatePriceFunction = async (form) => {
-        const { productId, price } = form;
-
+        const { productID, price } = form;
         try {
-            const data = await updatePrice([address, productId, price]);
-
-            console.log("Transaction Success", data);
-        } catch (error) {
-            console.log("fail transaction", error);
+          const data = await updatePrice({
+            args: [address, productID, ethers.utils.parseEther(price)],
+          });
+          console.info("contract call successs", data);
+        } catch (err) {
+          console.error("contract call failure", err);
         }
     };
 
     // 4:buy property
-    const { mutateAsync: buyProperty, isLoading: buyPropertyLoading } = useContractWrite(contract, "buyProperty");
+    // const { mutateAsync: buyProperty } = useContractWrite(contract, "buyProperty");
 
-    const buyPropertyFunction =async (from) => {
-        const { id } = from;
+    const buyPropertyFunction = async (buying) => {
+        const { productID, amount } = buying;
+        const money = ethers.utils.parseEther(amount);
+    
         try {
-            const data = await buyProperty({ args: [id, address] });
-            console.log("Purchase Successful", data);
-        } catch (error) {
-            console.log("Purchase failed", error);
+          const data = await contract.call("buyProperty", [productID, address], {
+            value: money.toString(),
+          });
+          console.info("contract call successs", data);
+          window.location.reload();
+        } catch (err) {
+          console.error("contract call failure", err);
         }
     };
 
     // 5:add review
-    // const { mutateAsync: addReview, isLoading: addReviewLoading } = useContractWrite(contract, "addReview");
+    const { mutateAsync: addReview } = useContractWrite(contract, "addReview");
 
-    // const addReviewFunction = async (from) => {
-    //     const {productId, rating, comment } = from;
+    const addReviewFunction = async (from) => {
+        const {productID, rating, comment } = from;
 
-    //     try {
-    //         const data = await addReview({
-    //             args: [productId, rating, comment, address],
-    //         });
-
-    //         console.log("Review added", data);
-    //     } catch (error) {
-    //         console.log("failed to add review", error);
-    //     }
-    // };
+        try {
+            const data = await addReview({
+              args: [productID, rating, comment, address],
+            });
+            console.info("contract call successs", data);
+            window.location.reload();
+          } catch (err) {
+            console.error("contract call failure", err);
+          }
+    };
 
     // 6: like review
-    // const { mutateAsync: likeReview, isLoading: likeReviewLoading } = useContractWrite(contract, "likeReview");
+    const { mutateAsync: likeReview} = useContractWrite(contract, "likeReview");
 
-    // const likeReviewFunction = async (from) => {
-    //     const {productId, reviewIndex} = from;
+    const likeReviewFunction = async (from) => {
+        const {productID, reviewIndex} = from;
 
-    //     try {
-    //         const data = await likeReview({
-    //             args: [productId, reviewIndex, address],
-    //         });
-    //         console.log("Commented Liked", data);
-    //     } catch (error) {
-    //         console.log("like failed", error);
-    //     }
-    // };
+        try {
+            const data = await likeReview({
+              args: [productID, reviewIndex, address],
+            });
+            console.info("contract call successs", data);
+            window.location.reload();
+          } catch (err) {
+            console.error("contract call failure", err);
+          }
+    };
 
     // 7:get all property
     const getPropertiesData = async () => {
+        
+        const properties = await contract.call("getAllProperties");
+    
+        
+        const listingPrice = await contract.call("listingPrice");
+        const chargePrice = ethers.utils.formatEther(listingPrice.toString());
+    
+        
+        const balance = await signer?.getBalance();
+        const userBalance = address
+          ? ethers.utils.formatEther(balance?.toString())
+          : "";
+        setUserBalance(userBalance);
+        const parsedProperties = properties.map((property, i) => ({
+          owner: property.owner,
+          title: property.propertyTitle,
+          description: property.description,
+          category: property.category,
+          price: ethers.utils.formatEther(property.price.toString()),
+          productID: property.productID.toNumber(),
+          reviewers: property.reviewers,
+          reviews: property.reviews,
+          image: property.images,
+          address: property.propertyAddress,
+        }));
+        return parsedProperties;
+      };
+
+    // 8: get highest product
+    const {data: getHighestratedProduct} = useContractRead(contract, "getHighestratedProduct");
+
+    // 9: getProductReviews
+    const getProductReviewsFunction = async (productId) => {
         try {
-            const properties = await contract.call("getAllProperty"); //fix needed
+            const getProductReviews = await contract.call("getProductReviews", [
+             productId,]);
+
+             const parsedReviews = getProductReviews?.map((review, i) => ({
+                reviewer: review.reviewer,
+                likes: review.likes.toNumber(),
+                comment: review.comment,
+                rating: review.rating,
+                productID: review.productId.toNumber(),
+                reviewIndex: review.reviewIndex.toNumber(),
+              }));    
+            return parsedReviews;
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+    // 10: get property
+    const getPropertyFunction = async (id) => {
+        const productID = id * 1;
+
+        try {
+            const propertyItem = await contract.call("getProperty", [productID]);
+
+            const property = {
+                productID: propertyItem?.[0].toNumber(),
+                owner: propertyItem?.[1],
+                title: propertyItem?.[3],
+                category: propertyItem?.[4],
+                description: propertyItem?.[7],
+                price: ethers.utils.formatEther(propertyItem?.[2].toString()),
+                address: propertyItem?.[6],
+                images: propertyItem?.[5],
+            };
+
+            return property;
+           
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // 11: getUserProperties
+    const getUserPropertiesFunction = async () => {
+        try {
+            const properties = await contract.call("getUserProperties", [address]);
 
             const parsedProperties = properties.map((property, i) => ({
                 owner: property.owner,
@@ -132,89 +236,50 @@ export const StateContextProvider = ({ children }) => {
                 description: property.description,
                 category: property.category,
                 price: ethers.utils.formatEther(property.price.toString()),
-                productId: property.productID.toNumber(),
-                // reviewers: property.reviewers,
-                // reviews: property.reviews,
-                Image: property.images,
+                productID: property.productID.toNumber(),
+                reviewers: property.reviewers,
+                reviews: property.reviews,
+                image: property.images,
                 address: property.propertyAddress,
-
             }));
-
-            return parsedProperties;
-            // console.log(properties);
-        } catch (error) {
-            console.log("Error while loading data", error);
-        }
-    };
-
-    // 8: get highest product
-    // const {data: getHighestratedProduct, isLoading: getHighestratedProductLoading} = useContractRead(contract, "getHighestratedProduct");
-
-    // 9: getProductReviews
-    // const getProductReviewsFunction = (productId) => {
-    //     try {
-    //         const {data: getProductReviews, isLoading: getProductReviewsLoading} = useContractRead(contract, "getProductReviews");
-
-            //   return getProductReviews, getProductReviewsLoading;  
-    //     } catch (error) {
-    //         console.log("failed to get property reviews", error);
-    //     }
-    // };
-
-
-    // 10: get property
-    const getPropertyFunction = (id) => {
-        try {
-            const { data: getProperty, isLoading: getPropertyLoading } = useContractRead("getProperty", [id]);
-            return getProperty, getPropertyLoading;
-        } catch (error) {
-            console.log("can not retrieve property", error);
-        }
-    };
-
-    // 11: getUserProperties
-    const getUserPropertiesFunction = () => {
-        try {
-            const { data: getUserProperties, isLoading: getUserPropertiesLoading } = useContractRead("getUserProperties", [address]);
-
-            return getUserProperties, getUserPropertiesLoading;
+            
+            return getUserProperties;
         } catch (error) {
             console.log("Error while getting user Property", error);
         }
     }
 
     // 12: get user review
-    // const getUserReviewsFunction = () => {
-    //     try {
-    //         const { data: getUserReviews, isLoading: getUserReviewsLoading } = useContractRead("getUserReviews", [address]);
+    const getUserReviewsFunction = () => {
+        try {
+            const { data: getUserReviews} = useContractRead("getUserReviews", [address]);
 
-    //         return getUserReviews, getUserReviewsLoading;
-    //     } catch (error) {
-    //         console.log("error", error);
-    //     }
-    // };
+            return getUserReviews;
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
 
 
     // 13: total property
-    const totalPropertyFunction = () => {
+    const totalPropertyFunction = async () => {
         try {
-            const { data: totalProperty, isLoading: totalPropertyLoading } = useContract(contract, "propertyIndex");
-
-            return totalProperty, totalPropertyLoading;
+            const totalProperty = await contract.call("propertyIndex")            
+            return totalProperty.toNumber();
         } catch (error) {
             console.log(error);
         }
     };
 
     // 14:total review
-    // const totalReviewsFunction = () => {
-    //     try {
-    //         const { data: totalReviewsCount, isLoading: totalReviewsCountLoading } = useContract(contract, "reviewsCounter");
-    //         return totalReviewsCount, totalReviewsCountLoading;
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
+    const totalReviewsFunction = async () => {
+        try {
+            const totalreviews = await contract.call("reviewsCounter");
+            return totalreviews.toNumber();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
 
 
@@ -229,23 +294,30 @@ export const StateContextProvider = ({ children }) => {
 
     return (
         <StateContext.Provider value={{
-            address, connect, contract, realEstate, 
-            createPropertyFunction, 
-            getPropertiesData, 
-            updatePriceFunction, 
-            updatePropertyFunction, 
-            // getProductReviewsFunction,
-            // likeReviewFunction,
-            // addReviewFunction,
-            // getHighestratedProduct,
-            // getUserReviewsFunction,
-            // totalReviewsFunction,
-            getPropertyFunction,
-            buyPropertyFunction, 
-            getUserPropertiesFunction,
-            totalPropertyFunction,
-            
-            }}>
+            //CONTRACT
+        address,
+        contract,
+        connect,
+        disconnect,
+        //PROPERTY
+        createPropertyFunction,
+        updatePropertyFunction,
+        updatePriceFunction,
+        buyPropertyFunction,
+        getPropertyFunction,
+        getUserPropertiesFunction,
+        totalPropertyFunction,
+        getPropertiesData,
+        //REVIEW
+        addReviewFunction,
+        likeReviewFunction,
+        getProductReviewsFunction,
+        getUserReviewsFunction,
+        totalReviewsFunction,
+        getHighestRatedProduct,
+        //STATE VARIABLE
+        userBalance,
+        }}>
             {children}
         </StateContext.Provider>
     );
