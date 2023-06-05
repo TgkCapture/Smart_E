dbf8469b9dd0d8d7c762e03453feecac3b6d4b36
TgkCapture <tawongakanyenda5@gmail.com>
2023-06-05 11:54:26 +0200

    mod: added new func

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,3 +1,5 @@
+import index from "@/pages";
+
 export const daysLeft = (deadline) => {
     const difference = new Date(deadline).getTime() - Date.now();
     const remainingDays = difference / (1000 * 3600 * 24);
@@ -19,4 +21,23 @@ export const checkIfImage = (url, callback) => {
 
     img.onload = () => callback(true);
     img.onerror = () => callback(false);
+};
+
+export const getTopCreators = (creators) => {
+    const finalCreators = [];
+
+    const finalResults = creators.reduce((index, currentValue) => {
+        (index[currentValue.owner] = index[currentValue.owner] || []).push(currentValue);
+
+        return index;
+    }, {});
+
+    Object.entries(finalResults).forEach((item) => {
+        const owner = item[0];
+        const total = item[1].map((newItem) => Number(newItem.price)).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+        finalCreators.push({ owner, total });
+    });
+
+    return finalCreators;
 };
