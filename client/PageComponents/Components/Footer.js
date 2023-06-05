ab4747d78d9b4391e813acbb8e121c8397a0168f
TgkCapture <tawongakanyenda5@gmail.com>
2023-06-05 11:48:07 +0200

    new: footer page added

---
+++ client/PageComponents/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">
+        <a href="/">
+          <img src="/images/House.png" alt="Smart E Logo" />
+        </a>
+      </div>
+      <div className="footer-text">
+        <p>Smart E</p>
+        <p>&copy; 2023 All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
