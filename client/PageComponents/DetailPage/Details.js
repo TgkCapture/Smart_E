36424e7a7f1539b1a8a47ac5c36201a240670c9f
TgkCapture <tawongakanyenda5@gmail.com>
2023-06-24 21:40:19 +0200

    Not in use

---
+++ client/PageComponents/DetailPage/Details.js
@@ -0,0 +1,49 @@
+// import React from 'react';
+
+// const Detail = () => {
+//     return (
+//          <div class="popup">
+//             <div class="property-pane">
+//                 <div class="pane-logo">
+//                     <img src="icons/image_0.jpg" alt="" />
+//                 </div>
+//                 <div class="pane-content">
+//                     <div class="pane-hide">
+//                         <button onclick=" ">
+//                             <img src="icons/close.png" alt="" />
+//                         </button>
+//                     </div>
+//                     <div class="pane-heading">
+//                         <p>Housing Heading </p>
+//                     </div>
+//                     <div class="pane-contain">
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur obcaecati repudiandae magnam! Aut nemo fugit nam perspiciatis dolore doloremque laborum, totam reiciendis eius nesciunt enim quae id deleniti aliquam soluta!</p>
+//                     </div>
+//                     <div class="pane-option">
+//                         <div class="pane-box">
+//                             <div class="pane-name">
+//                                 <p>Price</p>
+//                             </div>
+//                             <div class="pane-subname">
+//                                 <p>K10,000,000.00</p>
+//                             </div>
+//                         </div>
+//                         <div class="pane-box">
+//                             <div class="pane-name">
+//                                 <p>Location</p>
+//                             </div>
+//                             <div class="pane-subname">
+//                                 <p>Lilongwe</p>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div class="pane-button">
+//                         <button>Buy</button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div> 
+//     );
+// };
+
+// export default Detail;
