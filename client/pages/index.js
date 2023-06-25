0e2e65258de004f160bd29c2fabe4742edc3d9db
TgkCapture <tawongakanyenda5@gmail.com>
2023-06-25 12:37:36 +0200

    refactor: index page refactored

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,8 +9,6 @@ import Header from "@/PageComponents/Components/Header";
 
 import Hero from "@/PageComponents/HeroSection/Hero";
 
-
-
 const index = () => {
     const { address, connect, contract, realEstate, createPropertyFunction, getPropertiesData } = useStateContext();
 
@@ -59,8 +57,6 @@ const index = () => {
         if (contract) fetchProperty();
     }, [address, contract]);
 
-    // console.log(properties);
-
     // Categories
     const housing = [];
     const country = [];
@@ -88,60 +84,13 @@ const index = () => {
     }
     // creators
     const creators = getTopCreators(properties);
-    // console.log(creator);
-    // return (
-    //     <div>
-    //         <h1>{realEstate}</h1>
-    //         <button onClick={() => connect()}>Connect</button>
-    //         {/* <p>{address}</p> */}
-    //         <h1>Create</h1>
-    //         <form onSubmit={handleSubmit}>
-    //             <div>
-    //                 <input type="text" placeholder="propertyTitle" onChange={(e) => handleFromFieldChange("propertyTitle", e)} />
-    //             </div>
-
-    //             <div>
-    //                 <input type="text" placeholder="description" onChange={(e) => handleFromFieldChange("description", e)} />
-    //             </div>
-
-    //             <div>
-    //                 <input type="text" placeholder="category" onChange={(e) => handleFromFieldChange("category", e)} />
-    //             </div>
-
-    //             <div>
-    //                 <input type="number" placeholder="price" onChange={(e) => handleFromFieldChange("price", e)} />
-    //             </div>
-
-    //             <div>
-    //                 <input type="url" placeholder="images" onChange={(e) => handleFromFieldChange("images", e)} />
-    //             </div>
-
-    //             <div>
-    //                 <input type="text" placeholder="propertyAddress" onChange={(e) => handleFromFieldChange("propertyAddress", e)} />
-    //             </div>
-
-    //             <button type="submit">Submit</button>                
-
-    //         </form>
-    //     </div>
-    // );
-
+    
     return <div>
         <Header />
         <Hero />
-        {/* <Banner />
-        <Live properties={properties}/>
-        <Service />        
-        <Product properties={properties}/>
-        <TopSeller creators={creators}/>
-        <Collection housing={housing?.length} rental={rental?.length} farmhouse={farmhouse?.length} office={office?.length} />
-        <Copyright /> */}
         <Footer />
         
     </div>;
-
-    // return <h1>Index</h1>;
-
 };
 
 export default index;
